4ad72dc9ab4f8a18ad810d83ada683bb7b4c8d36
Savelii Sporyshev <savva.2004.02.27@gmail.com>
2023-06-25 00:14:35 +0300

    errors middleware fix

--- middlewares/handleErrors.js
+++ middlewares/handleErrors.js
@@ -1,11 +1,11 @@
-const { INTERNAL_SERVER_ERROR } = require('../errors/errors_constants');
+const { INTERNAL_SERVERE_ERROR } = require('../errors/errors_constants');
 const { SERVER_ERROR } = require('../utils/constants');
 
 const handleErrors = (err, req, res, next) => {
   if (err.statusCode) {
     res.status(err.statusCode).send({ message: err.message });
   } else {
-    res.status(INTERNAL_SERVER_ERROR).send({ message: SERVER_ERROR });
+    res.status(INTERNAL_SERVERE_ERROR).send({ message: SERVER_ERROR });
   }
   next();
 };
