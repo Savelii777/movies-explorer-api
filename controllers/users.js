b89d5612edb672b9df688c90576a7fa5f9f6127f
Savelii Sporyshev <savva.2004.02.27@gmail.com>
2023-06-25 00:09:23 +0300

    users controller fix

--- controllers/users.js
+++ controllers/users.js
@@ -11,70 +11,73 @@ const {
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-module.exports.createUser = async (req, res, next) => {
-  try {
-    const { name, email, password } = req.body;
-    const hash = await bcrypt.hash(password, 10);
-    const user = await User.create({ name, email, password: hash });
-    res.send({
+// post/signup
+module.exports.createUser = (req, res, next) => {
+  const {
+    name, email, password,
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      name,
+      email,
+      password: hash,
+    }))
+    .then((user) => res.send({
       name: user.name,
       _id: user._id,
       email: user.email,
+    }))
+    .catch((err) => {
+      if (err.code === 11000) {
+        next(new DuplicateError(USER_EXISTS));
+      } else if (err.name === 'ValidationError') {
+        next(new BadRequestError(USER_INCORRECT_DATA));
+      } else {
+        next(err);
+      }
     });
-  } catch (err) {
-    if (err.code === 11000) {
-      next(new DuplicateError(USER_EXISTS));
-    } else if (err.name === 'ValidationError') {
-      next(new BadRequestError(USER_INCORRECT_DATA));
-    } else {
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
+      res.send({ token });
+    })
+    .catch((err) => {
       next(err);
-    }
-  }
+    });
 };
 
-// post/signin
-module.exports.login = async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    const user = await User.findUserByCredentials(email, password);
-    const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
-    res.send({ token });
-  } catch (err) {
-    next(err);
-  }
+module.exports.getUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError(USER_NOT_FOUND);
+      }
+      return res.send(user);
+    })
+    .catch((err) => next(err));
 };
 
-module.exports.getUser = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.user._id);
-    if (!user) {
-      throw new NotFoundError(USER_NOT_FOUND);
-    }
-    res.send(user);
-  } catch (err) {
-    next(err);
-  }
-};
-
-module.exports.updateUser = async (req, res, next) => {
-  try {
-    const { name, email } = req.body;
-    const user = await User.findByIdAndUpdate(
-      req.user._id,
-      { name, email },
-      { new: true, runValidators: true },
-    );
-    if (!user) {
-      throw new NotFoundError(USER_NOT_FOUND);
-    }
-    res.send(user);
-  } catch (err) {
-    if (err.code === 11000) {
-      next(new DuplicateError(USER_EXISTS));
-    } else if (err.name === 'ValidationError') {
-      next(new BadRequestError(USER_UPDATE_INCORRECT_DATA));
-    } else {
-      next(err);
-    }
-  }
+module.exports.updateUser = (req, res, next) => {
+  const { name, email } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError(USER_NOT_FOUND);
+      }
+      return res.send(user);
+    })
+    .catch((err) => {
+      if (err.code === 11000) {
+        next(new DuplicateError(USER_EXISTS));
+      } else if (err.name === 'ValidationError') {
+        next(new BadRequestError(USER_UPDATE_INCORRECT_DATA));
+      } else {
+        next(err);
+      }
+    });
 };
