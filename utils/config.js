66b675c2e633d5810ed3e7ce7e25fe11a977e106
Savelii Sporyshev <savva.2004.02.27@gmail.com>
2023-06-25 00:22:50 +0300

    config fix

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,7 @@
 const { PORT = '3000' } = process.env;
-const { Database = 'mongodb://127.0.0.1:27017/moviesdb' } = process.env;
+const { DB = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 module.exports = {
   PORT,
-  Database,
+  DB,
 };
